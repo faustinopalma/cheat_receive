c3dc26a6296232e35d74e26c193975cb94a6e907
faustinopalma <39453908+faustinopalma@users.noreply.github.com>
2022-11-05 12:00:58 +0100

    remove the second iteration; add a signal for receipt and complete

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 radio.onReceivedNumber(function (receivedNumber) {
+    radio.sendString("R")
     basic.showNumber(receivedNumber + 1)
     for (let index = 0; index < receivedNumber + 1; index++) {
         pins.digitalWritePin(DigitalPin.P2, 1)
@@ -6,14 +7,7 @@ radio.onReceivedNumber(function (receivedNumber) {
         pins.digitalWritePin(DigitalPin.P2, 0)
         basic.pause(pause_vibration)
     }
-    basic.pause(3000)
-    for (let index = 0; index < receivedNumber + 1; index++) {
-        pins.digitalWritePin(DigitalPin.P2, 1)
-        basic.pause(vib_duration)
-        pins.digitalWritePin(DigitalPin.P2, 0)
-        basic.pause(pause_vibration)
-    }
-    radio.sendString("R")
+    radio.sendString("F")
 })
 let pause_vibration = 0
 let vib_duration = 0
