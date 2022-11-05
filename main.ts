f818ad8d6c8bd391645c0be5512d6b35ca237288
faustinopalma <39453908+faustinopalma@users.noreply.github.com>
2022-11-05 19:49:39 +0100

    add lock to avoid multiple concurrent input signals

--- main.ts
+++ main.ts
@@ -1,16 +1,22 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    radio.sendString("R")
-    basic.showNumber(receivedNumber + 1)
-    for (let index = 0; index < receivedNumber + 1; index++) {
-        pins.digitalWritePin(DigitalPin.P2, 1)
-        basic.pause(vib_duration)
-        pins.digitalWritePin(DigitalPin.P2, 0)
-        basic.pause(pause_vibration)
+    if (lock == 0) {
+        lock = 1
+        radio.sendString("R")
+        basic.showNumber(receivedNumber + 1)
+        for (let index = 0; index < receivedNumber + 1; index++) {
+            pins.digitalWritePin(DigitalPin.P2, 1)
+            basic.pause(vib_duration)
+            pins.digitalWritePin(DigitalPin.P2, 0)
+            basic.pause(pause_vibration)
+        }
+        lock = 0
+        radio.sendString("F")
     }
-    radio.sendString("F")
 })
 let pause_vibration = 0
 let vib_duration = 0
+let lock = 0
+lock = 0
 let message_number = 0
 basic.showNumber(message_number + 1)
 radio.setGroup(143)
